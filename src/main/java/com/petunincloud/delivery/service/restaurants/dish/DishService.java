a05fbd05f6eedb7b29652a357d7805f455ad8d43
macbookpro <stakhanin@gmail.com>
2026-08-17 21:32:02 +0700

    feat: добавил логи для DishService

--- src/main/java/com/petunincloud/delivery/service/restaurants/dish/DishService.java
+++ src/main/java/com/petunincloud/delivery/service/restaurants/dish/DishService.java
@@ -2,6 +2,8 @@ package com.petunincloud.delivery.service.restaurants.dish;
 
 import com.petunincloud.delivery.service.common.BaseService;
 import com.petunincloud.delivery.service.restaurants.dish.dto.DishResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +13,7 @@ import java.util.List;
 public class DishService extends BaseService<DishEntity, DishResponse, DishSearchFilter> {
     private final DishRepository dishRepository;
     private final DishMapper dishMapper;
+    private final static Logger log = LoggerFactory.getLogger(DishService.class);
 
     public DishService(
             DishRepository dishRepository,
@@ -21,14 +24,30 @@ public class DishService extends BaseService<DishEntity, DishResponse, DishSearc
     }
 
     public DishResponse getDishById(Long dishId) {
-        DishEntity entity = dishRepository.findById(dishId)
-                .orElseThrow(() -> new IllegalArgumentException("Dish not found: " + dishId));
+        log.info("Get dish by id: {}", dishId);
+        long startTime = System.currentTimeMillis();
 
-        return dishMapper.toResponse(entity);
+        try {
+            DishEntity entity = dishRepository.findById(dishId)
+                    .orElseThrow(() -> {
+                        log.warn("Dish not found: {}", dishId);
+                        return new IllegalArgumentException("Dish not found: " + dishId);
+                    });
+
+            long duration = System.currentTimeMillis() - startTime;
+            log.info("Success get dish by id: {}, duration={}ms", dishId, duration);
+
+            return dishMapper.toResponse(entity);
+
+        } catch (Exception e) {
+            log.error("Failed get dish by id: {}. Error: {}", dishId, e.getMessage());
+            throw e;
+        }
     }
 
     @Override
     protected List<DishEntity> findWithFilter(DishSearchFilter filter, Pageable pageable) {
+        log.debug("Searching dishes with filter: {}, pageable: {}", filter, pageable);
         return dishRepository.searchAllByFilter(
                 filter.name(),
                 filter.restaurantId(),
