bf1eb973f22d99855e1aff3246dd536e1237e791
macbookpro <stakhanin@gmail.com>
2026-08-18 16:02:44 +0700

    feat: добавил логи для AuthController

--- src/main/java/com/petunincloud/delivery/service/security/AuthController.java
+++ src/main/java/com/petunincloud/delivery/service/security/AuthController.java
@@ -4,11 +4,15 @@ import com.petunincloud.delivery.service.security.dto.AuthRequest;
 import com.petunincloud.delivery.service.security.dto.AuthResponse;
 import com.petunincloud.delivery.service.users.*;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,6 +32,7 @@ public class AuthController {
     private final PasswordEncoder passwordEncoder;
     private final RoleRepository roleRepository;
     private final UserMapper userMapper;
+    private final static Logger log = LoggerFactory.getLogger(AuthController.class);
 
     public AuthController(
             AuthenticationManager authenticationManager,
@@ -51,32 +56,69 @@ public class AuthController {
     public ResponseEntity<AuthResponse> login(
             @RequestBody AuthRequest request
     ) {
-        authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(request.email(), request.password())
-        );
+        log.info("POST /api/auth/login with request: {}", request);
+        long startTime = System.currentTimeMillis();
 
-        UserDetails userDetails = userDetailsService.loadUserByUsername(request.email());
-        String token = jwtUtils.generateToken(userDetails);
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(request.email(), request.password())
+            );
 
-        return ResponseEntity.ok(new AuthResponse(token));
+            UserDetails userDetails = userDetailsService.loadUserByUsername(request.email());
+            String token = jwtUtils.generateToken(userDetails);
+
+            long duration = System.currentTimeMillis() - startTime;
+            log.info("POST /api/auth/login with request: {} completed: {}, duration={}ms",
+                    request, HttpStatus.OK, duration);
+
+            return ResponseEntity.ok(new AuthResponse(token));
+
+        } catch (UsernameNotFoundException e) {
+            log.warn("POST /api/auth/login with request: {} failed: {}", request, e.getMessage());
+            throw e;
+
+        } catch (Exception e) {
+            log.error("POST /api/auth/login with request: {} unexpected error", request, e);
+            throw e;
+        }
     }
 
     @PostMapping("/register")
     public ResponseEntity<String> register(
             @RequestBody @Valid AuthRequest request
     ) {
-        if (userRepository.findByEmail(request.email()).isPresent()) {
-            return ResponseEntity.badRequest().body("Email already exists");
+        log.info("POST /api/auth/register with request: {}", request);
+        long startTime = System.currentTimeMillis();
+
+        try {
+            if (userRepository.findByEmail(request.email()).isPresent()) {
+                log.warn("POST /api/auth/register with request: {} failed: {}. Email already exists",
+                        request, HttpStatus.BAD_REQUEST);
+                return ResponseEntity.badRequest().body("Email already exists");
+            }
+
+            RoleEntity clientRole = roleRepository.findByName("ROLE_CLIENT")
+                    .orElseThrow(() -> {
+                        log.warn("POST /api/auth/register with request: {} failed: {}. Default role (ROLE_CLIENT) not found",
+                                request, HttpStatus.BAD_REQUEST);
+                        return new IllegalArgumentException("Default role not found");
+                    });
+
+            UserEntity user = userMapper.toEntity(request);
+            user.setPassword(passwordEncoder.encode(request.password()));
+            user.setRoles(Set.of(clientRole));
+
+            userRepository.save(user);
+
+            long duration = System.currentTimeMillis() - startTime;
+            log.info("POST /api/auth/register with request: {} completed: {}, duration={}ms",
+                    request, HttpStatus.OK, duration);
+
+            return ResponseEntity.ok("User registered successfully");
+
+        } catch (Exception e) {
+            log.error("POST /api/auth/register with request: {} unexpected error", request, e);
+            throw e;
         }
-
-        RoleEntity clientRole = roleRepository.findByName("ROLE_CLIENT")
-                .orElseThrow(() -> new IllegalArgumentException("Default role not found"));
-
-        UserEntity user = userMapper.toEntity(request);
-        user.setPassword(passwordEncoder.encode(request.password()));
-        user.setRoles(Set.of(clientRole));
-
-        userRepository.save(user);
-        return ResponseEntity.ok("User registered successfully");
     }
 }
